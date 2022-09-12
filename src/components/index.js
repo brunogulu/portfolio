b6fe21003380b0afad662d256b77c1664e9b00cc
Bruno Pittilini <bruno.pittilini@gmail.com>
2022-09-12 18:44:27 -0300

    routes

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export * from './Inicio';
+export * from './SobreMi';
+export * from './Habilidades';
+export * from './Contacto';
---
+++ src/routes/PortfolioRoutes.jsx
@@ -0,0 +1,15 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Contacto, Habilidades, Inicio, SobreMi } from '../components';
+
+export const PortfolioRoutes = () => {
+   return (
+      <Routes>
+         <Route path="/inicio" element={<Inicio />}></Route>
+         <Route path="/sobremi" element={<SobreMi />}></Route>
+         <Route path="/habilidades" element={<Habilidades />}></Route>
+         <Route path="/contacto" element={<Contacto />}></Route>
+
+         <Route path="/*" element={<Navigate to="/inicio" />} />
+      </Routes>
+   );
+};
